276c9b71439c2251017d61068d600921670c50ff
David Pineda <david.pineda@deparadise.com>
2021-02-12 16:05:01 -0500

    Setup SubMenu toggle and leave functionality

--- src/components/Navbar/SubMenu.js
+++ src/components/Navbar/SubMenu.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'gatsby';
 
 const SubMenu = ({ menu }) => {
-	console.log('>> TEST_USING_MENU:', menu);
+	// console.log('>> TEST_USING_MENU:', menu);
 
+	// STATE
+	const [active, setActive] = useState(false);
+	const [subMenuActiveClass, setSubMenuActiveClass] = useState('');
+
+	const closeSubMenu = () => {
+		setActive(false);
+	};
+
+	const toggleSubMenu = () => {
+		setActive(!active);
+	};
+	useEffect(() => {
+		active ? setSubMenuActiveClass('active') : setSubMenuActiveClass('');
+	});
+
+	// RENDER FLOW
 	let render;
 
 	if (menu.subMenu && menu.subMenu.length > 0) {
@@ -13,14 +29,18 @@ const SubMenu = ({ menu }) => {
 		} else {
 			// SUB-MENU /W TOP STATIC
 			render = (
-				<li className="navbar-item">
+				<li className="navbar-item" onMouseLeave={() => closeSubMenu()}>
 					<div
 						className="navbar-item-link menu-static-header"
 						aria-haspopup={true}
+						onClick={() => toggleSubMenu()}
 					>
 						{menu.name}
 					</div>
-					<ul className="navbar-submenu" aria-label="submenu">
+					<ul
+						className={`navbar-submenu ${subMenuActiveClass}`}
+						aria-label="submenu"
+					>
 						{menu.subMenu.map((subLink, i) => (
 							<li className="nav-bar-item sub-item" key={i}>
 								<Link
