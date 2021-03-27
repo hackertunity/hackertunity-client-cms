271af84cbd05d6d42dcf7b04c4b2d29912254f81
David Pineda <david.pineda@deparadise.com>
2021-03-27 10:32:48 -0400

    Register DefaultPagePreview and sideline About and Product page previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,15 +2,22 @@ import CMS from 'netlify-cms-app';
 import uploadcare from 'netlify-cms-media-library-uploadcare';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import IndexPagePreview from './preview-templates/IndexPagePreview';
+import DefaultPagePreview from './preview-templates/DefaultPagePreview';
+import BlogPostPreview from './preview-templates/BlogPostPreview';
+// import AboutPagePreview from './preview-templates/AboutPagePreview';
+// import ProductPagePreview from './preview-templates/ProductPagePreview';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('index', IndexPagePreview);
+
+// ABOUT US
+CMS.registerPreviewTemplate('our-mission', DefaultPagePreview);
+
+CMS.registerPreviewTemplate('blog', BlogPostPreview);
+
+// NOT USING RIGHT NOW
+// CMS.registerPreviewTemplate('about', AboutPagePreview)
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
