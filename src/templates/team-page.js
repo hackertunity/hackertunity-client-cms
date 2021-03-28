b5f029345e54733281770cce406759477573e35b
David Pineda <david.pineda@deparadise.com>
2021-03-28 11:53:18 -0400

    test background image imp on team page

--- src/templates/team-page.js
+++ src/templates/team-page.js
@@ -4,29 +4,52 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
 
-export const TeamPageTemplate = ({ title, content, contentComponent }) => {
+export const TeamPageTemplate = ({
+	title,
+	image,
+	content,
+	contentComponent,
+}) => {
 	const PageContent = contentComponent || Content;
 
+	console.log('>> TEST:', image);
+
 	return (
-		<section className="section section--gradient">
-			<div className="container">
-				<div className="columns">
-					<div className="column is-10 is-offset-1">
-						<div className="section">
-							<h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-								{title}
-							</h2>
-							<PageContent className="content" content={content} />
+		<div>
+			<div
+				className="full-width-image margin-top-0"
+				style={{
+					backgroundImage: `url(${
+						!!image.childImageSharp
+							? image.childImageSharp.fluid.src
+							: image
+					})`,
+					backgroundPosition: `top left`,
+					backgroundAttachment: `fixed`,
+				}}
+			></div>
+
+			<section className="section section--gradient">
+				<div className="container">
+					<div className="columns">
+						<div className="column is-10 is-offset-1">
+							<div className="section">
+								<h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+									{title} test affa
+								</h2>
+								<PageContent className="content" content={content} />
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</div>
 	);
 };
 
 TeamPageTemplate.propTypes = {
 	title: PropTypes.string.isRequired,
+	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 	content: PropTypes.string,
 	contentComponent: PropTypes.func,
 };
@@ -34,11 +57,14 @@ TeamPageTemplate.propTypes = {
 const TeamPage = ({ data }) => {
 	const { markdownRemark: post } = data;
 
+	console.log('>> TEST TEAM PAGE FM:', post);
+
 	return (
 		<Layout>
 			<TeamPageTemplate
 				contentComponent={HTMLContent}
 				title={post.frontmatter.title}
+				image={post.frontmatter.image}
 				content={post.html}
 			/>
 		</Layout>
@@ -57,6 +83,13 @@ export const TeamPageQuery = graphql`
 			html
 			frontmatter {
 				title
+				image {
+					childImageSharp {
+						fluid(maxWidth: 2048, quality: 100) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
 			}
 		}
 	}
