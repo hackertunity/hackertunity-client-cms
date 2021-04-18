4179375968bb2e2a1227f8ae9f8360adb1316964
David Pineda <david.pineda@deparadise.com>
2021-04-18 12:49:44 -0400

    Update DefaultPagePriview template to follow CMS preview loading convention

--- src/cms/preview-templates/DefaultPagePreview.js
+++ src/cms/preview-templates/DefaultPagePreview.js
@@ -2,15 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { DefaultPageTemplate } from '../../templates/default-page';
 
-const DefaultPagePreview = ({ entry }) => {
-	return <DefaultPageTemplate title={entry.getIn(['data', 'title'])} />;
+const DefaultPagePreview = ({ entry, getAsset, widgetFor }) => {
+	const data = entry.getIn(['data']).toJS();
+
+	if (data) {
+		return (
+			<DefaultPageTemplate
+				image={getAsset(data.image)}
+				title={data.title}
+				content={widgetFor('body')}
+			/>
+		);
+	} else {
+		return <div>Loading...</div>;
+	}
 };
 
 DefaultPagePreview.propTypes = {
 	entry: PropTypes.shape({
 		getIn: PropTypes.func,
 	}),
-	// getAsset: PropTypes.func,
+	getAsset: PropTypes.func,
 };
 
 export default DefaultPagePreview;
