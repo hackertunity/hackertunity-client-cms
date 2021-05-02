b61408d4655dd25db06317bb70a801a0e69af5d9
David Pineda <david.pineda@deparadise.com>
2021-05-02 13:02:15 -0500

    Feature/theme development team (#10)
    * Connect and prove light theme integration
    
    * Start theme_layout_alpha,sass
    
    * Touch up on nav and logo
    
    * add banner asset for landing
    
    * formating
    
    * Config for local BE server setup
    
    * Formatting
    
    * Start Project setup documentation with first notes on setting up local BE development
    
    * back out image for CMS intake
    
    * Reduce the index page to MVP fields
    
    * formatting
    
    * Register DefaultPagePreview and sideline About and Product page previews
    
    * Start the DefaultPagePreview
    
    * Add the Our Mission page to the cms admin collections register
    
    * Finish configuration for our-mission page
    
    * Register Acknowledgements page for admin editing
    
    * Start and register the TeamPagePreview and TeamPage templates
    
    * Start and register the TrainingResourcesPagePreview and TeamPage templates
    
    * Unregister the admin preview pages for about and products
    
    * Configure team organization admin edit convention
    
    * Configure Training resource admin edit convention
    
    * Modify admin config to support remaining theme dev
    
    * point our-team to correct page template
    
    * test background image imp on team page
    
    * starting theme prototype styles for default page layout
    
    * What I have for the team page template rendering
    
    * Connect the image, title, and content for TeamPagePreview
    
    * Use a list widget instead of boolean to support multiple group types in the template logic
    
    * Remove is committee bool data
    
    * Refactor naming convention of team page organization prop keys
    
    * Derp => TeamGroup
    
    * fix page vs preview render for member pictures
    
    * Rekey member props
    
    * derp
    
    * Start the components/team/group and confirm
    
    * Transfer out TeamGroup imp from team-page to component
    
    * classNames and styles for the team page

---
+++ src/components/team/group.js
@@ -0,0 +1,50 @@
+import React from 'react';
+// Addon dependencies imported
+// import { isEmpty } from '../../utils';
+// import HTMLparse from 'html-react-parser';
+
+// Utilized components imported
+// import SomeComp from './';
+
+const TeamGroup = ({ group }) => {
+	console.log('>> TEST RENDER TEAM_GROUP:', group);
+
+	// const handleSomething = () => {
+	// 	console.log('>>');
+	// };
+	// <SomeComp handleSomething={handleSomething} />
+
+	if (group.teamMembers) {
+		return (
+			<>
+				<h1 className="group team-name">{group.teamName}</h1>
+
+				{group.teamMembers.map((member, i) => {
+					let childImageSharp = member.profilePicture.childImageSharp;
+					let picSrc = childImageSharp
+						? childImageSharp.fixed.src
+						: member.profilePicture;
+					return (
+						<div className="group team-member" key={i}>
+							<img
+								className="group team-member-avatar"
+								src={picSrc}
+								alt={member.profileName}
+							/>
+							<h3 className="group team-member-title">
+								{member.profileName},&nbsp;{member.profileTitle}
+							</h3>
+							<p className="group team-member-profile">
+								{member.profileBlurb}
+							</p>
+						</div>
+					);
+				})}
+			</>
+		);
+	}
+
+	return <></>;
+};
+
+export default TeamGroup;
