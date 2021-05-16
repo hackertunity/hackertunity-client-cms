95987bc6a1c9f6ea65fd64d5932a26b6c629a13c
David Pineda <david.pineda@deparadise.com>
2021-05-16 10:53:06 -0400

    Page templating for training-resources page

--- src/templates/training-resources-page.js
+++ src/templates/training-resources-page.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
+
+import Layout from '../components/Layout';
+import PageBannerHead from '../components/page/pageBannerHead';
+import PageMainColumn from '../components/page/pageMainColumn';
 
 export const TrainingResourcesPageTemplate = ({
 	title,
+	image,
 	content,
 	contentComponent,
 }) => {
 	const PageContent = contentComponent || Content;
 
 	return (
-		<section className="section section--gradient">
-			<div className="container">
-				<div className="columns">
-					<div className="column is-10 is-offset-1">
-						<div className="section">
-							<h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-								{title}
-							</h2>
-							<PageContent className="content" content={content} />
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<div className="team-page">
+			<PageBannerHead image={image} title={title} />
+
+			<PageMainColumn>
+				<PageContent className="content" content={content} />
+			</PageMainColumn>
+		</div>
 	);
 };
 
