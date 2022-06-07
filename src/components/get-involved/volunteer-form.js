5e4d5dd15f8824b8b5b3f17a95a85287b52c6b15
David Pineda <david.pineda@deparadise.com>
2022-06-06 21:44:31 -0400

    xxx-xx: Edit - Update recaptcha

--- src/components/get-involved/volunteer-form.js
+++ src/components/get-involved/volunteer-form.js
@@ -15,7 +15,7 @@ const VolunteerForm = () => {
 	// STATE *
 	// HOW WOULD YOU HELP OR WHAT IS YOUR REL EXP?
 	return (
-		<form action="POST" data-netlify="true">
+		<form action="POST" data-netlify="true" data-netlify-recaptcha="true">
 			<div className="fields">
 				<div className="input-field">
 					<input type="text" name="first-name" id="first-name" placeholder="First Name *" required />
